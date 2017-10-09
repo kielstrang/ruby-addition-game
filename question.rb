6e631ad4bf62c906462a4d8567ce6a02fafafa5d
vagrant <kiel.strang@gmail.com>
2017-10-09 21:27:33 +0000

    Generate questions

--- question.rb
+++ question.rb
@@ -3,6 +3,10 @@
 # correct?
 module MathGame
   class Question
-
+    attr_reader :a, :b
+    def initialize
+      @a = 1 + rand(19)
+      @b = 1 + rand(19)
+    end
   end
 end
--- turn.rb
+++ turn.rb
@@ -1,18 +1,13 @@
-# Turn
-# takes [active_player, other_player]
-# creates a Question
-# displays question and takes input
-# reports correct/incorrect
-# reports winner or new scores
+require './question'
 
-# next_players
-# winner?
 module MathGame
   class Turn
     def initialize(players)
       @players = players
     end
     def do_turn
+      question = MathGame::Question.new
+      puts "What does #{question.a} plus #{question.b} equal?"
       @players[0].lives -= 1
       puts "P1: #{@players.find{|p| p.id == 1}.lives} vs P2: #{@players.find{|p| p.id == 2}.lives}"
     end
